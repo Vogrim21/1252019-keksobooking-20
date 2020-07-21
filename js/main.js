4da702fd89baec5931ccf243440c14c27d53da86
Vogrim <Fox210289@mail.ru>
2020-07-21 19:40:09 +0300

    Добавление попап

--- js/main.js
+++ js/main.js
@@ -17,6 +17,17 @@ var MAX_COORDS_Y = 630;
 var MIN_COORDS_X = 1;
 var MAX_COORDS_X = document.querySelector('.map').offsetWidth;
 
+var propertyType = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец'
+};
+
+var template = document.querySelector('#card')
+    .content
+    .querySelector('.map__card.popup');
+
 var map = document.querySelector('.map');
 var mapPins = map.querySelector('.map__pins');
 map.classList.remove('map--faded');
@@ -94,3 +105,91 @@ var generatePins = function (array) {
 };
 
 generatePins(generateAds(COUNT));
+
+var createCard = function (dto) {
+  var card = template.cloneNode(true);
+  var offer = dto.offer;
+
+  card.querySelector('.popup__avatar').setAttribute('src', dto.author.avatar);
+  card.querySelector('.popup__title').textContent = offer.title;
+  card.querySelector('.popup__text--address').textContent = offer.address;
+  card.querySelector('.popup__text--price').innerHTML = offer.price + ' &#x20bd;<span>/ночь</span>';
+  card.querySelector('.popup__type').textContent = propertyType[offer.type];
+  card.querySelector('.popup__text--capacity').textContent =
+    offer.rooms + ' ' + resolveDeclensionRoomsWord(offer.rooms) +
+    ' для ' + offer.guests + ' ' + resolveDeclensionGuestWord(offer.guests);
+  card.querySelector('.popup__text--time').textContent =
+    'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+
+  var featuresContainer = card.querySelector('.popup__features');
+  if (offer.features && offer.features.length > 0) {
+    resolveFeatures(featuresContainer, offer.features);
+  } else {
+    featuresContainer.hidden = true;
+  }
+
+  if (offer.description) {
+    card.querySelector('.popup__description').textContent = offer.description;
+  }
+
+  var photosContainer = card.querySelector('.popup__photos');
+  if (offer.photos && offer.photos.length > 0) {
+    resolvePhotos(photosContainer, offer.photos);
+  } else {
+    photosContainer.hidden = true;
+  }
+
+  function resolveFeatures(container, features) {
+    var testKeywordsExistence = createTestKeywordsExistenceFunc(features);
+    var filterNotMathedFeatureElements = function (element) {
+      var className = element.className;
+      return !testKeywordsExistence(className);
+    };
+    var notPresentFeatureElements = Array.from(container.children).filter(filterNotMathedFeatureElements);
+    notPresentFeatureElements.forEach(function (element) {
+      container.removeChild(element);
+    });
+
+    function createTestKeywordsExistenceFunc(keywords) {
+      var reString = keywords.join('|');
+      var re = new RegExp(reString);
+      return function testKeywordsExistenceFunc(string) {
+        return re.test(string);
+      };
+    }
+  }
+
+  function resolvePhotos(container, photoUrls) {
+    var photoListItemSample = container.querySelector('.popup__photo');
+    container.removeChild(photoListItemSample);
+    var photoListItems = photoUrls.map(function (url) {
+      var photoListItemElement = photoListItemSample.cloneNode(true);
+      photoListItemElement.setAttribute('src', url);
+      return photoListItemElement;
+    });
+    photoListItems.forEach(function (element) {
+      container.appendChild(element);
+    });
+  }
+
+  function resolveDeclensionRoomsWord(number) {
+    if (number >= 10 && number <= 20) {
+      return 'комнат';
+    }
+
+    var remainder = number % 10;
+    if (remainder === 1) {
+      return 'комната';
+    } else if (remainder >= 2 && remainder <= 4) {
+      return 'комнаты';
+    } else {
+      return 'комнат';
+    }
+  }
+
+  function resolveDeclensionGuestWord(number) {
+    return (number % 10 === 1) ? 'гостя' : 'гостей';
+  }
+};
+
+map.appendChild(createCard(generateAds));
